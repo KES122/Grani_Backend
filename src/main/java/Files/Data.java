af4a66bf0fc78b78bbcccf612824de222f0c3488
Alex <sasanazarov351@gmail.com>
2023-12-08 17:01:22 +0300

    API

--- src/main/java/Files/Data.java
+++ src/main/java/Files/Data.java
@@ -2,5 +2,6 @@ package Files;
 
 @lombok.Data
 public class Data {
-    private long id;
+    int id;
+    String name;
 }
--- src/main/java/controller/EventController.java
+++ src/main/java/controller/EventController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import model.Event;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -7,6 +9,7 @@ import repository.EventRepository;
 
 import java.util.List;
 
+@Api(description = "Иллюстрация работы")
 @RestController
 @RequestMapping("/api/events")
 public class EventController {
@@ -14,21 +17,27 @@ public class EventController {
     @Autowired
     private EventRepository eventRepository;
 
+    @ApiOperation("Найти всё")
     @GetMapping
     public List<Event> getAllEvents() {
         return eventRepository.findAll();
     }
 
+    @ApiOperation("Найти id")
     @GetMapping("/{id}")
     public Event getEventById(@PathVariable Long id) {
         return eventRepository.findById(id).orElse(null);
     }
 
+    @ApiOperation("Сохранить всё")
     @PostMapping
-    public Event createEvent(@RequestBody Event event) {
+    public Event createEvent(@RequestBody Event event)
+    {
         return eventRepository.save(event);
     }
 
+
+    @ApiOperation("Найти Имя, Дату и Локацию")
     @PutMapping("/{id}")
     public Event updateEvent(@PathVariable Long id, @RequestBody Event updatedEvent) {
         Event existingEvent = eventRepository.findById(id).orElse(null);
@@ -43,6 +52,7 @@ public class EventController {
         }
     }
 
+    @ApiOperation("Удалить id")
     @DeleteMapping("/{id}")
     public void deleteEvent(@PathVariable Long id) {
         eventRepository.deleteById(id);
